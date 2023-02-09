f6f75d3f3e897790f1eda7082655cfa8112a1ed2
MeganJS <247megamania@gmail.com>
2023-02-08 23:34:28 -0700

    fixed it

--- deployFiles.sh
+++ deployFiles.sh
@@ -9,21 +9,21 @@ do
     esac
 done
 
-if [[ -z "$key" || -z "$hostname" || -z "$service"]]; then
+if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
     printf "\nMissing required parameter.\n"
-    printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service> \n\n"
+    printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
     exit 1
 fi
 
 printf "\n----> Deploying files for $service to $hostname with $key\n"
 
-#step 1
-printf "\n----> Clear out the previous distribution  on the target. \n"
+# Step 1
+printf "\n----> Clear out the previous distribution on the target.\n"
 ssh -i "$key" ubuntu@$hostname << ENDSSH
 rm -rf services/${service}/public
 mkdir -p services/${service}/public
 ENDSSH
 
-#step 2
+# Step 2
 printf "\n----> Copy the distribution package to the target.\n"
 scp -r -i "$key" * ubuntu@$hostname:services/$service/public
